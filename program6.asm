90149252d01445410e44fc598407d60a61f50012
anderMatt <matt10.anderson@gmail.com>
2018-03-05 18:31:51 -0500

    Add project files.

---
+++ program6.asm
@@ -0,0 +1,25 @@
+TITLE Program 6      (program6.asm)
+
+; Author: Matthew Anderson			anderma8@oregonstate.edu
+; Course: CS 271 - Program 6                 Date: March 5, 2018
+; Description:
+
+INCLUDE Irvine32.inc
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	exit	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
